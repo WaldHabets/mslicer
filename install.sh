388fbd0424c77c075011c1f640fad9b4b92ef9b3
Wald Habets <wald.habets@protonmail.com>
2021-12-26 13:40:59 +0100

    created enviroment file

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+if [ -f .env ];
+then
+    export env $(cat .env | xargs)
+else
+    echo "Error: could not load enviroment variables!"
+fi
+
 if [ -z "$SUDO_USER" ];
 then
     echo "install requires sudo"
     exit
 fi
 
-if [ "$1" == "install" ];
-then
-    sudo mkdir -p "/usr/local/bin/"
-    sudo cp "./dist/mslicer" "/usr/local/bin/mslicer"
-fi
+TARGET="/usr/local/bin/"
 
-if [ "$1" == "remove" ];
-then
-    sudo rm -f "/usr/local/bin/mslicer"
+if [ "$1" == "install" ]; then
+    sudo mkdir -p "${TARGET}"
+    sudo cp "./dist/${DIST_NAME}" "${TARGET}${DIST_NAME}"
+elif [ "$1" == "remove" ]; then
+    sudo rm -f "${TARGER}${DIST_NAME}"
+else
+    echo "Invalid argument, use:"
+    echo "    install.sh install"
+    echo "    install.sh remove"
 fi
--- makefile
+++ makefile
@@ -5,6 +5,8 @@ SRC_DIR=src
 OBJ_DIR=obj
 DIST=dist
 
+include .env
+
 DEPS=$(wildcard $(SRC_DIR)/*.hpp)
 OBJS=$(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(wildcard $(SRC_DIR)/*.cpp))
 
@@ -16,7 +18,7 @@ LIBS=-fopenmp -lboost_program_options
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp $(DEPS) $(OBJ_DIR)
 	$(CC) -c -o $@ $< $(CFLAGS) $(LIBS)
 
-$(DIST)/mslicer: $(OBJS) $(DIST)
+$(DIST)/${DIST_NAME}: $(OBJS) $(DIST)
 	$(CC) -o $@ $(OBJS) $(LIBS)
 
 $(DIST):
