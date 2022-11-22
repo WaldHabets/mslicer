d38aa2cbd999b67cc4ac2e9456ffad9646408965
wald-habets <mail@waldhabets.eu>
2022-11-22 19:02:22 +0100

    building blocks for automatic layer hiding

--- src/xml.cpp
+++ src/xml.cpp
@@ -1,11 +1,33 @@
 #include "xml.hpp"
 #include <string>
 #include <iostream>
+#include <boost/foreach.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
 using namespace boost;
 namespace pt = boost::property_tree;
+
+/**
+ * Parses the floating value of the specified attribute
+ * If the attribute cannot 
+ * @param root
+ * The root of the svg file
+ * @param attribute
+ * The attribute path pointing at the floating value
+ * */
+float parse_non_null_positive_float_attr(const pt::ptree& root, const std::string attribute) {
+    try {
+        const std::string dim_string = root.get<std::string>(attribute);
+        const float dim = strtof(dim_string.c_str(), NULL);
+        if (dim == 0) {
+            return PROPERTY_IS_MISSING;
+        }
+        return dim;
+    } catch (boost::wrapexcept<boost::property_tree::ptree_bad_path>) {
+        return PROPERTY_IS_MISSING;
+    }
+}
 
 SvgDimension parse_dimension(const std::string& path) {
     SvgDimension dim;
@@ -15,38 +37,34 @@ SvgDimension parse_dimension(const std::string& path) {
     pt::ptree root;
     pt::read_xml(path, root);
 
-    bool missing_width = false;
-    bool missing_height = false;
-
-    std::string height_string = "";
-    std::string width_string = "";
-
-    try {
-        width_string = root.get<std::string>("svg.<xmlattr>.width");
-        dim.width = strtof(width_string.c_str(), NULL);
-        if (dim.width == 0) {
-            dim.width = PROPERTY_IS_MISSING;
-        }
-    } catch (boost::wrapexcept<boost::property_tree::ptree_bad_path>) {
-        dim.width = PROPERTY_IS_MISSING;
-    }
-
-    try {
-        height_string = root.get<std::string>("svg.<xmlattr>.height");
-        dim.height = strtof(width_string.c_str(), NULL);
-        if (dim.width == 0) {
-            dim.height = PROPERTY_IS_MISSING;
-        }
-    } catch (boost::wrapexcept<boost::property_tree::ptree_bad_path>) {
-        dim.height = PROPERTY_IS_MISSING;
-    }
-
-    if (missing_width || missing_height) {
-        throw new MissingPropertyException(missing_width, missing_height);
-    }
-    
-
-    std::cout << height_string << " - " << width_string << std::endl;
+    dim.width = parse_non_null_positive_float_attr(root, "svg.<xmlattr>.width");
+    dim.height = parse_non_null_positive_float_attr(root, "svg.<xmlattr>.height");
 
     return dim;
+}
+
+void set_display(const std::string& path, const std::string& id, bool display = true) {
+    pt::ptree root;
+    pt::read_xml(path, root);
+
+    const pt::ptree& groups = root.get_child("g");
+
+    BOOST_FOREACH(ptree::value_type& group, groups) 
+    {
+        const pt::ptree& attributes = group.second.get_child("<xmlattr>");
+        const std::string group_id = group.second.get<std::string>("<xmlattr>.id");
+
+        if (group_id.compare(id) == 0) {
+            
+
+            std::string display_style = display ? "display:inline" : "display:none";
+            try {
+                const std::string style = group.second.get<std::string>("<xmlattr>.style");
+
+            } catch (boost::wrapexcept<boost::property_tree::ptree_bad_path>) {
+                const std::string style = display ? "display:inline" : "display:none";
+                
+            }
+        }
+    }
 }
--- src/xml.hpp
+++ src/xml.hpp
@@ -26,7 +26,28 @@ private:
     bool m_missing_height = false;
 };
 
+/**
+ * Find the width and hight on a svg file
+ * @param path
+ * The root of the svg file
+ * @return
+ * A struct containing the width and height
+ * @throws MissingPropertyException
+ * if either the width or height are not defined on the svg
+ * */
 SvgDimension parse_dimension(const std::string& path);
+
+/**
+ * Set the style attribute "display" to either "display:inline" or "display:none" on a layer of the svg
+ * Each layer should be defined as a "g" tag with the id property as a direct child of the "svg" root tag.
+ * @param path
+ * The root of the svg file
+ * @param id
+ * The id of the layer
+ * @param display
+ * The value of the "display" attribute
+ * */
+void set_display(const std::string& path, const std::string& id, bool display = true);
 
 
 #endif  // GUARD_XML_HPP
